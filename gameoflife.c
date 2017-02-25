0f4895251324317ef0a6df4b30482445fa9360ce
I-Spangler <ingridespangler@gmail.com>
2017-02-25 16:58:48 -0300

    Add files via upload

--- gameoflife.c
+++ gameoflife.c
@@ -1,148 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-int** criamatriz(int linhas, int colunas, FILE * fp){ //cria matriz direto do arquivo alocando memoria com duas linhas e duas colunas a mais
-    int i,j;
-    int **gi = (int**)malloc((linhas+2) * sizeof(int*));
-    for(i = 0; i<linhas+2; i++)
-        gi[i] = (int*)malloc((colunas+2) * sizeof(int));
-    for(i = 0; i<linhas+2; i++){
-        for(j = 0; j<colunas+2; j++){
-            gi[i][j] = 0;
-            }
-        }
-    for(i = 1; i<=linhas; i++){
-        for(j = 1; j<=colunas; j++){
-            fscanf(fp, "%d ", &gi[i][j]);
-        }
-    }
-    return gi;
+#include<unistd.h>
+#define ROWS 34
+#define COLS 119
+#define SPACE 32 
+
+char neighbors(char gi[][COLS], int i, int j){
+    int vizinho = 0;
+	int dead = 0;
+    if(gi[i-1][j-1] == ' ')
+        dead++;
+	else if(gi[i-1][j-1]> vizinho)
+		vizinho = gi[i-1][j-1];
+    if(gi[i-1][j] == ' ')
+        dead++;
+	else if(gi[i-1][j]> vizinho)
+		vizinho = gi[i-1][j];
+    if(gi[i-1][j+1] == ' ')
+        dead++;
+	else if(gi[i+1][j+1]> vizinho)
+		vizinho = gi[i+1][j+1];
+    if(gi[i][j-1] == ' ')
+        dead++;
+	else if(gi[i][j-1]> vizinho)
+		vizinho = gi[i][j-1];
+    if(gi[i][j+1] == ' ')
+        dead++;
+	else if(gi[i][j+1]> vizinho)
+		vizinho = gi[i][j+1];
+    if(gi[i+1][j-1] == ' ')
+        dead++;
+	else if(gi[i+1][j-1]> vizinho)
+		vizinho = gi[i+1][j-1];
+    if(gi[i+1][j] == ' ')
+        dead++;
+	else if(gi[i+1][j]> vizinho)
+		vizinho = gi[i+1][j];
+    if(gi[i+1][j+1] == ' ')
+        dead++;
+	else if(gi[i+1][j+1]> vizinho)
+		vizinho = gi[i+1][j+1];
+//da rulez
+	if (dead > 6) //less than two live neighbors. dies.
+		vizinho = SPACE;
+    else if(dead < 5) //more than three live neighbors. dies. or stay dead.
+		vizinho = SPACE;
+    else if(gi[i][j] == SPACE && dead == 6)//dead and two neighbors. stay dead
+		vizinho = SPACE;
+    else if(gi[i][j] != SPACE && (dead == 6 || dead == 5)) //alive and well
+		vizinho = gi[i][j];
+    return vizinho; //last condition, dead and three neighbors. rule 5
 }
-int contavizinhos(int i, int j, int** gi){ //acha o numero de vizinhos
-    int vizinhos = 0;
-    if(gi[i-1][j-1] == 1)
-        vizinhos++;
-    if(gi[i-1][j] == 1)
-        vizinhos++;
-    if(gi[i-1][j+1] == 1)
-        vizinhos++;
-    if(gi[i][j-1] == 1)
-        vizinhos++;
-    if(gi[i][j+1] == 1)
-        vizinhos++;
-    if(gi[i+1][j-1] == 1)
-        vizinhos++;
-    if(gi[i+1][j] == 1)
-        vizinhos++;
-    if(gi[i+1][j+1] == 1)
-        vizinhos++;
-    return vizinhos;
+
+void nextgen(char prev[][COLS]){
+    //here
+	int i, j; 
+	int nex[ROWS][COLS];
+    for(j = 0; j < ROWS; j++){
+        for(i = 0; i < COLS; i++){
+            nex[j][i] = SPACE;
+        }
+    }
+    for(j = 1; j < ROWS-1; j++){
+        for(i = 1; i < COLS-1; i++){
+			nex[j][i] = neighbors(prev, j, i); // what's this supposed to do? why ==? no idea lol 
+            // it writes the letter of the new generation in this cell.
+        }
+    }
+
+    for(j = 0; j < ROWS; j++){
+        for(i = 0; i < COLS; i++){
+			prev[j][i] = nex[j][i];
+        }
+    }
 }
-int** proximageracao(int** gi, int linhas, int colunas){
-    int i, j;
-    int **gf = (int**)malloc((linhas+2) * sizeof(int*));
-    for(i = 0; i<linhas+2; i++)
-        gf[i] = (int*)malloc((colunas+2) * sizeof(int));
-    for(i = 0; i<linhas+2; i++){
-        for(j = 0; j<colunas+2; j++){
-            gf[i][j] = 0;
-            }
-        }
-    for(i = 1; i <= linhas; i++){
-        for(j = 1; j <= colunas; j++){
-            gf[i][j] = contavizinhos(i, j, gi); //gf fica sendo uma matriz onde cada célula é o número de vizinhos em gi
-        }
-    }
-    for(i = 1; i <= linhas; i++){ //a célula da matriz gf recebe 1 ou 0 seguindo as regras do exercicio
-        for(j = 1; j <= colunas; j++){
-            if(gi[i][j] == 1){
-                if(gf[i][j] < 2 || gf[i][j] > 3)
-                    gf[i][j] = 0;
-                else if(gf[i][j] == 3 || gf[i][j] == 2)
-                    gf[i][j] = 1;
-            }
-            if(gi[i][j] == 0)
-                if(gf[i][j] == 3)
-                    gf[i][j] = 1;
-                else gf[i][j] = gi[i][j];
+
+int main(int argc, char** argv){
+    int i, j, test;
+    FILE * input;
+    input = fopen(argv[1], "r");
+    if(!input)
+        printf("no file!");
+    char grid[ROWS][COLS];
+    printf("%c", grid[40][40]);
+    for(j = 0; j < ROWS; j++){
+        for(i = 0; i < COLS; i++){
+            grid[j][i] = SPACE;
         }
     }
-    return gf;
-}
-int main(void){
-    char c, str[100];
-    int n, **gi, i, j, linhas = 0, colunas = 0;
-    FILE *fp, *gff;
-    printf("\n%c%c%c%c%c%c  JOGO DA VIDA  %c%c%c%c%c%c\n\nDIGITE o nome do arquivo (incluindo .txt).\n",178,178,177,177,176,176,176,176,177,177,178,178);
-    gets(str);
-    fp = fopen(str, "r");
-    if(fp == NULL){
-        printf("Erro ao abrir o arquivo. Digite ENTER para encerrar.\n");
-        getchar();
-        return;
-    }
-    printf("DIGITE 'S' PARA COMECAR");
-    fscanf(fp, "%d %d", &linhas, &colunas);
-    //cria a matriz e preenche
-    gi = criamatriz(linhas, colunas, fp);
-    fclose(fp);
-    while(1){
-        scanf("%c", &c);
-       switch(c){
-        case('n'):{
-                gff = fopen("geracaofinal.txt", "w");
-                for(i = 1; i<=linhas; i++){
-                    for(j = 1; j<=colunas; j++){
-                        fprintf(gff, "%d ", gi[i][j]);
-                    }
-                    fprintf(gff, "\n");
-                }
-                fclose(gff);
-            for(i = 0; i< linhas; i++)
-                free(*gi);
-            free(gi);
-            return 0;
-        }
-        case('s'):{
-            printf("%c", 201);
-            for(j = 1; j<=colunas; j++)
-                printf("%c", 205);
-            printf("%c\n", 187);
-            for(i = 1; i<=linhas; i++){
-                printf("%c", 186);
-                for(j = 1; j<=colunas; j++){
-                        if(gi[i][j] == 0)
-                            printf(" ");
-                        if(gi[i][j] == 1)
-                            printf("*");
-                }
-                printf("%c\n", 186);
-            }
-            printf("%c", 200);
-            for(j = 1; j<=colunas; j++)
-                printf("%c", 205);
-            printf("%c\n", 188);
-            gi = proximageracao(gi, linhas, colunas);
-            printf("DESEJA GERAR A PROXIMA GERACAO?\nSE SIM, DIGITE 'S'\nSE NAO, DIGITE 'N'\nDIGITE 'L' PARA UM LOOP INFINITO.");
-            break;
-        }
-        case('l'):{
-            while(1){
-                for(i = 1; i<=linhas; i++){
-                    printf("%c", 186);
-                for(j = 1; j<=colunas; j++){
-                        if(gi[i][j] == 0)
-                            printf(" ");
-                        if(gi[i][j] == 1)
-                            printf("*");
-                }
-                printf("%c\n", 186);
-            }
-            usleep(41000);
-            system("cls");
-            gi = proximageracao(gi, linhas, colunas);
+
+    for(j = 1; j < ROWS-1; j++){
+        for(i = 1; i < COLS-1; i++){
+            fscanf(input, "%c", &grid[j][i]);
+            printf("%c", grid[j][i]);
+			if(grid[j][i] == '\n'){ 
+                i = 0;
+                break;
             }
         }
+       printf("\n");
+        
     }
+
+    int k;
+    for(k = 0; k < 875; k++){
+        nextgen(grid);
+        for(j = 0; j < ROWS; j++){
+            for(i = 0; i < COLS; i++){
+                printf("%c", grid[j][i]);
+            }
+
+            printf("\n");
+        }
+        usleep(41000);
+        system("clear");
+    } 
+
+    FILE *output = fopen("output.txt", "w"); 
+    for(j = 0; j < ROWS; j++){
+        for(i = 0; i < COLS; i++){
+            fprintf(output,"%c", grid[j][i]);
+        }
+
+        fprintf(output, "\n");
     }
+    return 0;
 }
